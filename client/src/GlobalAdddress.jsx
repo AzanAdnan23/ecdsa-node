61f074479eb36f9d8fc67788c99fb36ffa41d2a3
Azan <azanbutt2311@gmail.com>
2023-07-01 17:27:51 +0500

    final commit

--- client/src/GlobalAdddress.jsx
+++ client/src/GlobalAdddress.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // Add import for axios
-import server from './server';
 
 function GlobalAddresses() {
   const [balances, setBalances] = useState(null);
@@ -8,12 +7,8 @@ function GlobalAddresses() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const copiedUrl = 'http://localhost:3042/balances';
-
-        const response = await axios.get(copiedUrl); // Use axios.get() instead of copiedUrl
-
-        const data = response.data; // Extract data from response
-
+        const response = await axios.get('http://localhost:3042/balances');
+        const data = response.data;
         setBalances(data);
       } catch (error) {
         console.error(error);
@@ -23,35 +18,45 @@ function GlobalAddresses() {
     fetchData();
   }, []);
 
-  const formatPublicKey = (publicKey) => {
-    // Format the publicKey here according to your preference
-    return publicKey;
-  };
+  useEffect(() => {
+    const updateBalance = async () => {
+      try {
+        const response = await axios.get('http://localhost:3042/balances');
+        const data = response.data;
+        setBalances(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  const formatAmount = (amount) => {
-    // Format the amount here according to your preference
-    return amount;
-  };
+    const interval = setInterval(() => {
+      updateBalance();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div style={{ backgroundColor: '#fff', border: '1px solid #ccc', padding: '20px', borderRadius: '8px', maxWidth: '600px', margin: '0 auto' }}>
       <h1>Global Balances</h1>
       {balances ? (
         <div style={{ marginTop: '20px' }}>
-          <div style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', marginBottom: '10px' }}>
-            <span style={{ width: '50%' }}>Public Key</span>
-            <span style={{ width: '50%' }}>Amount</span>
-          </div>
-          {Object.entries(balances).map(([publicKey, amount]) => (
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px' }} key={publicKey}>
-              <span style={{ marginRight: '10px' }}>{formatPublicKey(publicKey)}</span>
-              <span style={{ marginLeft: '10px' }}>{formatAmount(amount)}</span>
-            </div>
-          ))}
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            <li style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', marginBottom: '10px' }}>
+              <span style={{ flex: 1 }}>Public Key</span>
+              <span style={{ flex: 1 }}>Amount</span>
+            </li>
+            {Object.entries(balances).map(([publicKey, amount]) => (
+              <li style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px' }} key={publicKey}>
+                <span>{publicKey}</span>
+                <span>{amount}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       ) : (
         <p>Loading balances...</p>
-      )}
+      )}  
     </div>
   );
 }
